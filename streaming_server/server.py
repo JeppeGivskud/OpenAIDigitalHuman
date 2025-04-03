64e2890cd6e2530457ef59fb47592420e1110575
JeppeGivskud <jeppefirring@gmail.com>
2025-04-03 10:12:04 +0200

    Added a mock server functions

--- streaming_server/server.py
+++ streaming_server/server.py
@@ -1,17 +1,30 @@
 import os
 import sys
 from concurrent import futures
+from unittest.mock import MagicMock
+
+sys.modules["omni"] = MagicMock()
+sys.modules["omni.audio2face"] = MagicMock()
+sys.modules["omni.audio2face.common"] = MagicMock()
+# Import the mocked functions
+from omni.audio2face.common import log_error, log_info, log_warn
 
 import grpc
 import numpy as np
 from grpc import _common, _server
-from omni.audio2face.common import log_error, log_info, log_warn
 
-if 0:  # Use this only during development, this helps to hot-reload grpc/protobuf properly
+
+if (
+    0
+):  # Use this only during development, this helps to hot-reload grpc/protobuf properly
     os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
     from google.protobuf.internal import api_implementation
 
-    log_warn("DEV MODE (turn it off in production): api_implementation.Type() == {}".format(api_implementation.Type()))
+    log_warn(
+        "DEV MODE (turn it off in production): api_implementation.Type() == {}".format(
+            api_implementation.Type()
+        )
+    )
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, ROOT_DIR)
@@ -47,7 +60,9 @@ class Audio2FaceServicer(audio2face_pb2_grpc.Audio2FaceServicer):
                 return audio2face_pb2.PushAudioResponse(success=False, message=str(e))
         if self._audio_end_callback is not None:
             try:
-                self._audio_end_callback(instance_name, block_until_playback_is_finished)
+                self._audio_end_callback(
+                    instance_name, block_until_playback_is_finished
+                )
             except RuntimeError as e:
                 return audio2face_pb2.PushAudioResponse(success=False, message=str(e))
         log_info("PushAudio request -- DONE")
@@ -57,12 +72,19 @@ class Audio2FaceServicer(audio2face_pb2_grpc.Audio2FaceServicer):
         first_item = next(request_iterator)
         if not first_item.HasField("start_marker"):
             return audio2face_pb2.PushAudioResponse(
-                success=False, message="First item in the request should containt start_marker"
+                success=False,
+                message="First item in the request should containt start_marker",
             )
         instance_name = first_item.start_marker.instance_name
         samplerate = first_item.start_marker.samplerate
-        block_until_playback_is_finished = first_item.start_marker.block_until_playback_is_finished
-        log_info("PushAudioStream request: [instance_name = {} ; samplerate = {}]".format(instance_name, samplerate))
+        block_until_playback_is_finished = (
+            first_item.start_marker.block_until_playback_is_finished
+        )
+        log_info(
+            "PushAudioStream request: [instance_name = {} ; samplerate = {}]".format(
+                instance_name, samplerate
+            )
+        )
         if self._audio_start_callback is not None:
             try:
                 self._audio_start_callback(instance_name, samplerate)
@@ -75,11 +97,15 @@ class Audio2FaceServicer(audio2face_pb2_grpc.Audio2FaceServicer):
                 try:
                     self._push_chunk_callback(instance_name, audio_data)
                 except RuntimeError as e:
-                    return audio2face_pb2.PushAudioResponse(success=False, message=str(e))
+                    return audio2face_pb2.PushAudioResponse(
+                        success=False, message=str(e)
+                    )
 
         if self._audio_end_callback is not None and instance_name is not None:
             try:
-                self._audio_end_callback(instance_name, block_until_playback_is_finished)
+                self._audio_end_callback(
+                    instance_name, block_until_playback_is_finished
+                )
             except RuntimeError as e:
                 return audio2face_pb2.PushAudioResponse(success=False, message=str(e))
         log_info("PushAudioStream request -- DONE")
@@ -95,15 +121,24 @@ class StreamingServer:
 
     def start(self, audio_start_callback, push_chunk_callback, audio_end_callback):
         log_info("StreamingServer -- START")
-        self._server = grpc.server(futures.ThreadPoolExecutor(max_workers=self._max_workers))
-        audio2face_pb2_grpc.add_Audio2FaceServicer_to_server(
-            Audio2FaceServicer(audio_start_callback, push_chunk_callback, audio_end_callback), self._server
+        self._server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=self._max_workers)
         )
-        success = _server._add_insecure_port(self._server._state, _common.encode(self._address))
+        audio2face_pb2_grpc.add_Audio2FaceServicer_to_server(
+            Audio2FaceServicer(
+                audio_start_callback, push_chunk_callback, audio_end_callback
+            ),
+            self._server,
+        )
+        success = _server._add_insecure_port(
+            self._server._state, _common.encode(self._address)
+        )
         while success == 0:
             self._port = str(int(self._port) + 1)
             self._address = f"[::]:{self._port}"
-            success = _server._add_insecure_port(self._server._state, _common.encode(self._address))
+            success = _server._add_insecure_port(
+                self._server._state, _common.encode(self._address)
+            )
         self._server.start()
         log_info("StreamingServer -- START -- Done")
 
