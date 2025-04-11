3a38f9a5dbff7f9550c5ae4840450c77334f3459
JeppeGivskud <jeppefirring@gmail.com>
2025-04-11 11:35:52 +0200

    Rosie now waits until openai has streamed all the audio and then she speaks

--- Pipeline.py
+++ Pipeline.py
@@ -9,6 +9,9 @@ from agents import Agent
 from Audio import (
     record_audio_while_pressed,
     handle_audio_stream,
+    load_audio_file,
+    send_audio_to_audio2face_server,
+    play_audio,
 )
 
 
@@ -20,25 +23,51 @@ def create_pipeline(agent: Agent):
     )
 
 
-async def run_pipeline(pipeline, omniverse=False):
+async def run_pipeline(
+    pipeline, renderFace=False, useTokens=False, prerecordedaudiopath=None
+):
     while True:
         """Runs the voice pipeline with recorded audio."""
 
         print("Recording audio... (Press and hold the spacebar to stop recording)")
-        buffer = record_audio_while_pressed()
-        audio_input = AudioInput(buffer=buffer)
+        mic_recording = record_audio_while_pressed()
+        audio_input = AudioInput(buffer=mic_recording)
         print("Audio recorded.")
 
-        # result = await pipeline.run(audio_input)
-        result = await pipeline.run(audio_input)
+        if useTokens:
+            # Send the audio input to the pipeline
+            print("Sending audio to Audio2Face...")
+            result = await pipeline.run(audio_input)
 
-        # Stream the result
-        await handle_audio_stream(
-            result,
-            send_audio=omniverse,  # Set to True if you want to send audio to Audio2Face
-            instance_name="/World/audio2face/PlayerStreaming",
-            url="localhost:50051",
-        )
+            # Stream the result
+            await handle_audio_stream(
+                result,
+                renderFace=renderFace,  # Set to True if you want to send audio to Audio2Face
+                instance_name="/World/audio2face/PlayerStreaming",
+                url="localhost:50051",
+            )
+        else:
+            if prerecordedaudiopath != None:
+                print("loading file - ", prerecordedaudiopath)
+                file, samplerate = load_audio_file("streaming_server/testrec.wav")
+            else:
+                print("Using recorded audio...")
+                file = mic_recording
+                samplerate = 24000
+
+            if renderFace:
+                # Send the audio input to the pipeline
+                print("Sending audio to Audio2Face...")
+                send_audio_to_audio2face_server(
+                    file,
+                    samplerate=samplerate,
+                    instance_name="/World/audio2face/PlayerStreaming",
+                    url="localhost:50051",
+                )
+            else:
+                # Play the audio locally
+                print("Playing audio locally...")
+                play_audio(file, samplerate)
 
         # Wait for user input to continue
         input("Press Enter to run the pipeline again...")
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import asyncio
+import sys
 from My_Agents import Rosie_dk_agent
 from Pipeline import create_pipeline, run_pipeline
 
 
 async def main():
+    renderFace = False
+    useTokens = False
+
+    if len(sys.argv) >= 3:
+        renderFace = sys.argv[1].lower() == "true"
+        useTokens = sys.argv[2].lower() == "true"
+
     pipeline = create_pipeline(Rosie_dk_agent)
-    await run_pipeline(pipeline, omniverse=True)
+    print(renderFace, useTokens)
+    await run_pipeline(
+        pipeline, renderFace=renderFace, useTokens=useTokens, prerecordedaudiopath=None
+    )
 
 
 if __name__ == "__main__":
