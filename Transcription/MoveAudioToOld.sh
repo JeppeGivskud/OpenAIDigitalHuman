2526bdb8618345fffaaab8798e3fb33bb3f1a826
JeppeGivskud <jeppefirring@gmail.com>
2025-05-01 09:36:27 +0200

    Now moves metadata

--- Transcription/MoveAudioToOld.sh
+++ Transcription/MoveAudioToOld.sh
@@ -7,9 +7,15 @@ OLD_DIR="$BASE_DIR/../saved_audio/old"
 SESSIONS_DIR="$BASE_DIR/../saved_audio/Sessions"
 OLD_TIMESTAMP_DIR="$OLD_DIR/$(date +"%Y%m%d_%H%M%S")"
 
+METADATA_DIR="$BASE_DIR/../saved_data/Sessions"
+OLD_DATA_DIR="$BASE_DIR/../saved_data/old"
+OLD_TIMESTAMP_DIR_DATA="$OLD_DATA_DIR/$(date +"%Y%m%d_%H%M%S")"
+
 # Ensure the old directory exists
 mkdir -p "$OLD_DIR"
+mkdir -p "$OLD_DATA_DIR"
 mkdir -p "$OLD_TIMESTAMP_DIR"
+mkdir -p "$OLD_TIMESTAMP_DIR_DATA"
 
 # Move contents from ../Transcription/AudioFiles/ to ../saved_audio/old/
 if [ -d "$TRANSCRIPTION_DIR" ]; then
@@ -26,6 +32,15 @@ if [ -d "$SESSIONS_DIR" ]; then
     find "$SESSIONS_DIR" -mindepth 1 -maxdepth 1 -exec mv {} "$OLD_TIMESTAMP_DIR/" \; || echo "No files or folders to move."
 else
     echo "Directory $SESSIONS_DIR does not exist. Skipping..."
+fi
+
+# Move contents from ../saved_data/Sessions to ../saved_data/old/timestamp
+if [ -d "$METADATA_DIR" ]; then
+    echo "Moving files and folders from $METADATA_DIR to $OLD_TIMESTAMP_DIR_DATA..."
+    mkdir -p "$OLD_TIMESTAMP_DIR_DATA"  # Ensure the target directory exists
+    find "$METADATA_DIR" -mindepth 1 -maxdepth 1 -exec mv {} "$OLD_TIMESTAMP_DIR_DATA/" \; || echo "No files or folders to move."
+else
+    echo "Directory $METADATA_DIR does not exist. Skipping..."
 fi
 
 echo "File moving completed."
